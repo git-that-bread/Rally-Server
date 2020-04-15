5938e488b64efc034fe7d9775edc2d4a939e97ec
Nataly Carbonell <natcarbonell97@gmail.com>
2020-04-15 17:43:19 -0400

    Add getUser and embed the underlyingUser in the response

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,14 +5,17 @@
  */
 
 const router = require('express').Router();
+const middleware = require('./middleware/middleware');
 
 const authRouter = require('./auth');
 router.use('/auth', authRouter);
 
 const adminRouter = require('./admin');
+adminRouter.use('/', middleware.checkAuthentication);
 router.use('/admin', adminRouter);
 
 const volRouter = require('./volunteer');
+volRouter.use('/', middleware.checkAuthentication);
 router.use('/volunteer', volRouter);
 
 const orgRouter = require('./organization');
--- routes/api/middleware/middleware.js
+++ routes/api/middleware/middleware.js
@@ -1,34 +1,44 @@
 const login = require('./loginMiddleware');
 const signup = require('./signupMiddleware');
+const moment = require('moment');
+const jwt = require('jsonwebtoken');
+const User = require('../../../models/user.model')
 
 const checkAuthentication = async (req, res, next) => {
-  let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-  if(!token) {
-    return res.status(404).send({code: 'TOKEN_MISSING', message: 'An authorization token was not provided in the request header.' });
-  }
-  if (token.startsWith('Bearer ')) {
-    // Remove Bearer from string
-    token = token.slice(7, token.length);
+  console.log("check authorization middleware")
+  if (!req.headers.authorization) {
+    return res.status(401).send({code: 'TOKEN_MISSING', message: 'An authorization token was not provided in the request header.' });
   }
 
-  if (token) {
-    try {
-      const decoded = await jwt.verify(token, process.env.JWT_KEY);
-      req.decoded = decoded;
-      const user = await User.findById(decoded.sub.userId);
-      console.log("user authenticated: ");
-      console.log(user);
-      if (!user){
-        return res.status(404).send({code: 'USER_NOT_EXISTS', message: 'User does not exist.' });
-      } else {
-        req.user = user;
-        req.decoded = decoded;
-        next();
-      }
-    } catch (error) {
-      next(error);
-    }
+  var token = req.headers.authorization.split(' ')[1];
+  var payload = null;
+
+  try {
+    payload = jwt.verify(token, process.env.JWT_KEY);
   }
+  catch (err) {
+    return res.status(401).send({code: 'TOKEN_INVALID', message: 'An invalid token was provided.' });
+  }
+
+
+if (payload.exp <= moment().unix()) {
+    return res.status(401).send({code: 'TOKEN_EXPIRED', message: 'Token has expired.' });
+  }
+
+  console.log(payload)
+  User.findById(payload.userId).select("-password").then((user) => {
+        console.log(user)
+        if(!user) {
+          return res.status(404).send({code: 'USER_NOT_EXISTS', message: 'User does not exist.' });
+        }
+        req.user = user;
+        next();
+  }).catch((error) => {
+    next(error);
+  });
+
+
+  
 };
 
 module.exports = {
