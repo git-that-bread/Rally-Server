af35e37cdc63eb3f5b8eb6fdbe645ab9c2f2019f
Nataly Carbonell <natcarbonell97@gmail.com>
2020-04-20 21:25:10 -0400

    use DB_URI env

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
 
 module.exports = {
-    mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds115263.mlab.com:15263/volunteerinfo`
+    mongoURI: `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URI}`
 }
